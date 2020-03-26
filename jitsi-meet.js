8a1d3dea66721c747adcdcdf7f1967b4cd2d5f17
Pietro Rampazzo <pietro@rampazzo.co>
2020-03-26 22:44:06 +0100

    Add getter data-url from each row

--- jitsi-meet.js
+++ jitsi-meet.js
@@ -1,9 +1,20 @@
-const domain = 'meet.jit.si';
+const domain = "meet.jit.si";
+
 const options = {
-    roomName: 'dojopd-all',
-    width: '100%',
-    height: '100%',
-    parentNode: document.querySelector('#meet')
+  roomName: "dojopd-all",
+  width: "100%",
+  height: "100%",
+  parentNode: document.querySelector("#meet")
 };
 const api = new JitsiMeetExternalAPI(domain, options);
 
+const rows = document.querySelectorAll(".row");
+
+// for each row
+for (const row of rows) {
+  row.addEventListener("click", function(event) {
+    // TODO: scoll up
+    room = row.getAttribute("data-url");
+    alert(room);
+  });
+}
