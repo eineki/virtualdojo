bee5203b65d3372aa5b2c38c7ee3f052a1627424
Pietro Rampazzo <pietro@rampazzo.co>
2020-03-27 08:39:38 +0100

    Add on click change room

--- jitsi-meet.js
+++ jitsi-meet.js
@@ -1,20 +1,25 @@
 const domain = "meet.jit.si";
 
 const options = {
-  roomName: "dojopd-all",
+  roomName: "dojopd-all", // initial room
   width: "100%",
   height: "100%",
   parentNode: document.querySelector("#meet")
 };
-const api = new JitsiMeetExternalAPI(domain, options);
+var api = new JitsiMeetExternalAPI(domain, options);
 
+// get all .row div
 const rows = document.querySelectorAll(".row");
 
-// for each row
+// for each row add on click listener
 for (const row of rows) {
-  row.addEventListener("click", function(event) {
+  row.addEventListener("click", function(e) {
     // TODO: scoll up
-    room = row.getAttribute("data-url");
-    alert(room);
+
+    // destroy existing meet
+    api.dispose();
+
+    options.roomName = row.getAttribute("data-url"); // change room name
+    api = new JitsiMeetExternalAPI(domain, options); // generate new meet
   });
 }
