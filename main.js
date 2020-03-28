74327c8635c985ea207542c43998684575cddb67
Giacomo Magisano <eineki@liberosoftware.net>
2020-03-28 12:51:02 +0100

    added UI resizing behaviour

--- main.js
+++ main.js
@@ -11,7 +11,20 @@
        /* interfaccia */
        document.querySelector("#board").addEventListener('click', hnd_teamswitch);
        document.body.addEventListener('unload', ev=> api.dispose());
+       document.getElementById("shrink").addEventListener('click', hnd_changeZoom);
+       document.getElementById("enlarge").addEventListener('click', hnd_changeZoom);
 
+function hnd_changeZoom(ev) {
+		ev.preventDefault();
+		var step = 1 * ev.target.dataset.delta;
+		var limit = 1 * ev.target.dataset.limit;
+		var actual = 1 * document.body.dataset.fsize;
+		if (step < 0 && actual <= limit) return;
+		if (step > 0 && actual >= limit) return;
+		actual += step;
+		document.body.dataset.fsize = actual;
+		document.body.style="font-size: " + actual + "px;";
+}
 
 function hnd_teamswitch(ev) {
 	if (ev.target.matches('a.command')) {
