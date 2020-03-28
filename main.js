1f7345723a65ef5062dba2618494bf322df1d89e
Giacomo Magisano <eineki@liberosoftware.net>
2020-03-28 11:43:52 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+		/* jitsi setup */
+        const domain = 'meet.jit.si';
+        const options = {
+            roomName: 'dojopd-all',
+			requireDisplayName: true,
+            // width: document.querySelector("#meet").clientWidth,
+            // height: document.querySelector("#meet").clientHeight,
+            parentNode: document.querySelector('#meet')
+	    };
+        let api = null;  // new JitsiMeetExternalAPI(domain, options);
+       /* interfaccia */
+       document.querySelector("#board").addEventListener('click', hnd_teamswitch);
+       document.body.addEventListener('unload', ev=> api.dispose());
+
+
+function hnd_teamswitch(ev) {
+	if (ev.target.matches('a.command')) {
+	  ev.preventDefault();
+	  var self = ev.target;
+	  if (api !== null) {
+		api.executeCommand('hangup');
+		api.dispose();
+	  }
+	  if (!document.body.classList.contains("meet")) {
+		document.body.classList.add("meet");
+	  }
+	  var nextRoomOpt = options;
+	  nextRoomOpt.roomName = self.dataset.room;
+	  var locator = document.getElementById("locator");
+	  if (self.classList.contains('zoo')) {
+		  locator.hinnerHTML = "";
+		  document.body.className = "meet";
+	  } else {
+		  locator.innerHTML=self.dataset.descr;
+  	  	  document.body.classList.add('zoo');
+		  document.body.classList.add(self.href.split("#")[1]);
+  	  }
+	  api = new JitsiMeetExternalAPI(domain, nextRoomOpt);
+   }
+ }
+
+
